bdf5a5e32f559f9a720b2920cbb760eb9600a295
nursimacelik <33669453+nursimacelik@users.noreply.github.com>
2021-05-15 21:28:23 +0300

    Update 462project_step2_Solis.py

--- 462project_step2_Solis.py
+++ 462project_step2_Solis.py
@@ -4,6 +4,11 @@ import os
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 import string
+from nltk import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
 
 # preprocesses the given string
 # converts to lower case, removes punctuation and stop words
@@ -17,22 +22,20 @@ stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you"
               "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
               "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
               "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-              "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "film", "movie", "story", "one", "character", "time", "movies"]
+              "few", "more", "most", "other", "some", "such", "only", "own", "same", "so", "than",
+              "too", "very", "s", "t", "can", "will", "just", "don", "should", "now", "film", "movie", "story", "one",
+              "character", "time", "movies", "br", "39", "quot"]
+
+stop_words2 = ["br", "quot", "39"]
 
 def clean_data(text):
   lower_case = text.lower()
-  wh_special_char = lower_case.translate(str.maketrans('','',string.punctuation))
+  wh_special_char = lower_case.translate(str.maketrans(string.punctuation,' '*len(string.punctuation)))
   cleaned_data = ""
   for word in wh_special_char.split():
-    if word not in stop_words:
-      cleaned_data += " " + word
+    if word not in stop_words2:
+      cleaned_data += " " + lemmatizer.lemmatize(word)
   return cleaned_data
-
-
-
-
-
 
 
 
@@ -46,6 +49,9 @@ FILE_PATH = sys.argv[2]
 with open(pkl_filename, "rb") as file:
   clf = pickle.load(file)
 
+with open("count.pkl", "rb") as file:
+  count = pickle.load(file)
+
 data = []
 labels = []
 for file in os.listdir(FILE_PATH):
@@ -57,7 +63,7 @@ for file in os.listdir(FILE_PATH):
     f.close()
 
 
-count = CountVectorizer()
+#count = CountVectorizer()
 bag = count.transform(data)
 print(bag.toarray())
 #print(count.vocabulary_)
@@ -65,3 +71,67 @@ print(bag.toarray())
 
 prediction = clf.predict(bag.toarray())
 print("".join(prediction))
+
+
+
+count = 0
+count_n = 0
+count_p = 0
+count_z = 0
+label_n = 0
+label_p = 0
+label_z = 0
+tp = 0
+tn = 0
+tz = 0
+N = len(labels)
+for i in range(N):
+  if prediction[i] == 'P':
+    count_p += 1
+    if labels[i] == prediction[i]:
+      tp += 1
+  elif prediction[i] == 'N':
+    count_n += 1
+    if labels[i] == prediction[i]:
+      tn += 1
+  elif prediction[i] == 'Z':
+    count_z += 1
+    if labels[i] == prediction[i]:
+      tz += 1
+
+  if labels[i] == 'P':
+    label_p += 1
+  elif labels[i] == 'N':
+    label_n += 1
+  elif labels[i] == 'Z':
+    label_z += 1
+
+accuracy = (tp + tz + tn)*100/N
+print("Train accuracy: " + str(accuracy))
+  
+precision_p = tp / count_p
+precision_n = tn / count_n
+precision_z = tz / count_z
+
+recall_p = tp / label_p
+recall_n = tn / label_n
+recall_z = tz / label_z
+
+print("Precisions (positive, negative, neutral): ")
+print(precision_p)
+print(precision_n)
+print(precision_z)
+
+print("Recalls (positive, negative, neutral): ")
+print(recall_p)
+print(recall_n)
+print(recall_z)
+
+print("Macro average accuracy: ")
+print((recall_p + recall_n + recall_z) / 3)
+
+print("Macro average precision: ")
+print((precision_p + precision_n + precision_z) / 3)
+
+print("Macro average recall: ")
+print((recall_p + recall_n + recall_z) / 3)
